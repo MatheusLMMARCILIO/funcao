30a18d1ae76a83e8379f3cebcb9b73482337bdfa
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-03-11 14:16:19 -0300

    ok

---
+++ for/script.js
@@ -0,0 +1,12 @@
+// for (let i= 0; i <= 5; i++) {
+//     console.log(`linha ${i}`)
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i)
+// }
+
+for (let i = 1; i >= 10; i++) {
+    console.log(`5 x ${i} = ${5 * i}`)
+}
+
---
+++ maneirasDeFazerfunctions/script.js
@@ -0,0 +1,17 @@
+function NomeDaFuncao() {
+    console.log('conteúdo')
+}
+
+const nomeDaVariavel = function () {
+    console.log('Conteúdo')
+}
+
+const objFunction = {
+    funcao: function () {
+        console.log('conteúdp')
+    }
+}
+
+const arrowFunction = () => {
+    console.log('conteúdo')
+}
